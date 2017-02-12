4bf4d9bdbb5904d3274117e34c8b8245e2415447
jluccisano <joseph.luccisano@gmail.com>
2017-02-12 17:22:00 +0100

    fix delete

--- client/modules/route/scripts/routeBuilder.js
+++ client/modules/route/scripts/routeBuilder.js
@@ -509,7 +509,7 @@ var routeBuilder = {};
 			return lastLatLng;
 		};
 
-		this._getLastMarker = function() {
+		this.getLastMarker = function() {
 			return this._markers[this._markers.length - 1];
 		};
 
@@ -536,7 +536,7 @@ var routeBuilder = {};
 							new google.maps.Size(16, 16)
 						);
 
-						var lastMarker = this._getLastMarker();
+						var lastMarker = this.getLastMarker();
 						lastMarker.icon = segmentIcon;
 
 					}
