cf040576be4bbf2c26f95549b3e8d9d5f600f5c2
jluccisano <joseph.luccisano@gmail.com>
2017-02-12 17:22:00 +0100

    change password

--- scripts/mongo_backup.sh
+++ scripts/mongo_backup.sh
@@ -2,6 +2,6 @@
 
 COMPRESS_FILE=/home/jluccisano/nextrun_backups/compressed/nextrun-backup-$(date +%Y-%m-%d).tar.gz
 
-mongodump -h  192.95.25.173:27017 -d nextrun  -o /home/jluccisano/nextrun_backups
+mongodump -h  ip:port -d nextrun  -o /home/jluccisano/nextrun_backups
 tar -zcvf $COMPRESS_FILE /home/jluccisano/nextrun_backups/nextrun
-curl -T $COMPRESS_FILE ftp://81.56.136.120/Volume_2/backup/backup_nextrun/ --user jluccisano:malili011004 
+curl -T $COMPRESS_FILE ftp://ip/Volume_2/backup/backup_nextrun/ --user login:password
